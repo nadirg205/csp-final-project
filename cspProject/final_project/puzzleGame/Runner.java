93905f441b615fdc49034117cd238710f6b48365
12629767 <12629767@WH261ST4.wayneschools.net>
2023-05-25 12:12:26 -0400

    a

---
+++ cspProject/final_project/puzzleGame/Runner.java
@@ -0,0 +1,23 @@
+package puzzleGame;
+
+import processing.core.PApplet;
+
+public class Runner extends PApplet {
+
+	public static void main(String[] args) {
+//		PApplet.main("puzzleGame.Runner");
+		System.out.println("");
+	}
+	
+	public void settings(){
+		size(640, 480);
+	}
+	
+	public void setup() {
+		
+	}
+	
+	public void draw() {
+		
+	}
+}
---
+++ cspProject/src/module-info.java
@@ -0,0 +1,10 @@
+/**
+ * 
+ */
+/**
+ * @author 12629767
+ *
+ */
+module cspProject {
+	requires core;
+}
